c02bdcc045a055fe41a99630f0bcd8de590f97dd
Alex King <alex@likening.co.uk>
2024-01-16 16:58:18 +0000

    update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Electromagnetism
+
+### Electric Force
+
+$$F = \frac{1}{4 \pi \epsilon_0} \frac{q_1 q_2}{r^2} \hat{i}$$
--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 from vpython import *
 import numpy as np
-from atomic import PROTON
-
-
-
+from atomic import PROTON, System
 
 
 def main():
-    p1 = replace(PROTON)
+    p1 = PROTON
     p1.pos = vector(0, 0, 0)
     p1.q = 1
 
-    p2 = replace(PROTON)
+    p2 = PROTON
     p2.pos = vector(1, 0, 0)
     p2.q = 2
 
-    p4 = replace(PROTON)
+    p4 = PROTON
     p4.pos = vector(0.375, 0.65, 0)
     p4.q = -4
 
-    temp = net_force([p1, p2, p4])
+    system = System()
+
+    temp = system.net_e_force([p1, p2, p4])
     print(temp)
     print(mag(temp))
 
